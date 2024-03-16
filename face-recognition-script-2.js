73c34cb666ab336b274113f53ab8557fc72dcbc1
Raymund Hermoso <124145913+dev-raymund@users.noreply.github.com>
2024-03-16 14:32:39 +0800

    WIP: Debug camera

--- face-recognition-script-2.js
+++ face-recognition-script-2.js
@@ -9,7 +9,7 @@ Promise.all([
 
 function startVideo() {
   // navigator.getUserMedia(
-  navigator.mediaDevices.getUserMedia(
+  navigator.mediaDevices(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
